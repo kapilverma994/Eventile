6dd77a0e43a0ef1de3509d2f80dad013e0338ada
kapilverma994 <74894827+kapilverma994@users.noreply.github.com>
2021-03-22 10:34:20 +0530

    sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,8 @@ Route::group(['namespace'=>'Auth'],function(){
 
     Route::post('register','RegisterController@register');
     Route::post('login','LoginController@login');
+    Route::post('me',function(){
+        return auth()->user();
+    })->middleware('auth:sanctum');
 });
     
