7184cf691f86488c97d57e0559ce09673db569a7
kapilverma123 <kapil.verma@adsandurl.com>
2021-03-24 16:54:45 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,9 @@
 <?php
-Route::group(['namespace'=>'Auth'],function(){
-
-    Route::post('register','RegisterController@register');
-    Route::post('login','LoginController@login');
-    Route::post('logout','LoginController@logout')->middleware('auth:sanctum');
-    Route::post('me',function(){
+Route::group(['namespace' => 'Auth'], function () {
+    Route::post('register', 'RegisterController@register');
+    Route::post('login', 'LoginController@login');
+    Route::post('logout', 'LoginController@logout')->middleware('auth:sanctum');
+    Route::post('me', function () {
         return auth()->user();
     })->middleware('auth:sanctum');
 });
-    
